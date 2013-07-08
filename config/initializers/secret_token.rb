539c18755fe52446d4f2d19f5f283b2043c70a7a
luzerno <irfan.1989@gmail.com>
2013-07-08 19:40:51 -0400

    init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caramel::Application.config.secret_key_base = '25bb4f8b0b27d53cd509dd8cecdff1e358502b6c1dd7f192341288820e03705a21a868baa18892203414b787017859a15054add8e03a07d1d42db8fe267359ff'

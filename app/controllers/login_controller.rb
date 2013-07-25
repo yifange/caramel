8d9454db697aced29814cb194da7dc1e05d5d0f5
DengJinqiu <deng.jinqiu@gmail.com>
2013-07-25 00:39:48 -0400

    finish login page

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,5 @@
+class LoginController < ApplicationController
+  def index
+		render :layout => false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Caramel::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "admins#new", :as => "signup"
-#  resources :schools, :instruments, :course_types, :programs, :attendence_markings, :calendar_markings, :attendences, :teachers, :sessions, :users, :admins, :staffs, :regions
  
 	get 'people' => 'people#index'
 	get 'program' => 'program#index'
 	get 'calendar' => 'calendar#index'
+	root 'login#index'
 
  # root :to => "schools#index"
   # The priority is based upon order of creation: first created -> highest priority.

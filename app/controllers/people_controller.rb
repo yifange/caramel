b5a4772f858f04c5bd5cb4c04b69e3c0c3c145a3
ccya <csyccya@hotmail.com>
2013-07-30 18:36:36 -0400

    Before merge with Jinqiu

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,8 @@
 class PeopleController < ApplicationController
   def staff
 		verify_user
+        # @staffs=Staff.pluck(:first_name, :last_name, :email, :Region)
+        @staffs = Staff.all
 	end
 
   def teachers
@@ -11,4 +13,9 @@ class PeopleController < ApplicationController
   def students
 		verify_user
   end
+
+  def get_firstname(last_name)
+         Staff.where(:last_name).first_name
+  end
+
 end

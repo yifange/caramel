4488df1b4551fd02454a2d0753c25d9475f63834
luzerno <irfan.1989@gmail.com>
2013-08-12 14:19:07 -0400

    wut

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 class Student < ActiveRecord::Base
   has_many :enrollments
   has_many :progams, :through => :enrollments
-
+  has_many :courses, :through => :rosters
+  has_many :rosters
   include People
 
 

818241fc29344819c01247cee4091a15ffc3cb0e
luzerno <irfan.1989@gmail.com>
2013-08-28 23:06:10 -0400

    bootstarp flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,12 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error then "alert alert-error"
+      when :alert then "alert alert-error"
+    end
+  end
 end

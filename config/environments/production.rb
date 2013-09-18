6d69d83d0219ccaa2c98e651b04d7ccb9383754d
luzerno <irfan.1989@gmail.com>
2013-09-17 23:54:20 -0400

    added seesion_page.css to precompile pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Caramel::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( session_page.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

b4a7a34796c24edf489586421b821a0ffcf8f3a2
luzerno <irfan.1989@gmail.com>
2013-09-27 00:00:13 -0400

    huh

--- lib/calendar_builder.rb
+++
@@ -1,5 +0,0 @@
-require "calendar_builder/table_helper.rb"
-require "calendar_builder/calendar_helper.rb"
-
-# ActionView::Base.send :include, TableHelper
-# ActionView::Base.send :include, CalendarHelper

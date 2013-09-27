5121e9a1d2567cff97810f841b3244e2c398c5e6
luzerno <irfan.1989@gmail.com>
2013-09-27 01:17:22 -0400

    color in annual view

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,2 +1,14 @@
 module CalendarsHelper
+  def day_marking(objs)
+    marking = ""
+    for obj in objs
+      m = obj.available if obj.respond_to? :available
+      if marking.empty?
+        marking = m ? "available" : "unavailable"
+      else
+        marking = "mix" if m != marking
+      end
+    end
+    marking
+  end
 end
